b64744ccc4502e048cf0a5455051ed595c2a1059
Erick Gachoka <gachokaeric819@gmail.com>
2022-04-24 06:55:54 +0300

    adds custom config properties

---
+++ src/main/java/com/example/demo/config/Application.java
@@ -0,0 +1,12 @@
+package com.example.demo.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "application")
+public class Application {
+    private String welcome;
+}
--- src/main/java/com/example/demo/web/WelcomeController.java
+++ src/main/java/com/example/demo/web/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.example.demo.web;
 
+import com.example.demo.config.Application;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class WelcomeController {
+    private final Application application;
+
+    public WelcomeController(Application application) {
+        this.application = application;
+    }
+
     @GetMapping
     public ResponseEntity<String> welcome() {
-        return ResponseEntity.ok("Welcome to Smart Ci-CD Demo");
+        return ResponseEntity.ok(application.getWelcome());
     }
 }
