ace9758fffccb5d9aad053deaa17e3c703e758e8
Erick Gachoka <erick.gachoka@smart.local>
2022-04-28 10:43:45 +0300

    prevents users with duplicate usernames

--- src/main/java/com/example/demo/security/User.java
+++ src/main/java/com/example/demo/security/User.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -25,6 +26,7 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
 
     private String password;
